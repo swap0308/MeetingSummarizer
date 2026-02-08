bfcf784bfdfdb9adda63566ea1a985c5192736e2
abhayyraj <dwivedirajabhay@gmail.com>
2026-02-08 21:32:47 +0530

    added api call on video select

--- summarizer-ui/src/App.tsx
+++ summarizer-ui/src/App.tsx
@@ -1,15 +1,7 @@
 import Chat from './components/Chat'
 import Sidebar from './components/Sidebar'
-import { useEffect } from 'react'
-import { baseUrl, serviceRequest } from './utils';
 
 function App() {
-
-  useEffect(() => {
-    serviceRequest(`${baseUrl}/breeds`, {})
-    ?.then(res => console.log(res, '//////////'))
-    .catch(err => console.log(err, '//////////'));
-  }, []);
 
   return (
     <div className='h-[100vh] flex gap-[20px] p-[20px]'>
